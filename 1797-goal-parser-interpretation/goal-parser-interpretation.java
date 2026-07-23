2be665d46295cb563ae114c2d7e663387f9ffad7
Archana Mishra <archanamishra082005@gmail.com>
2026-07-23 10:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (32.57%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String interpret(String command) {
+       int s=command.length();
+       StringBuilder sb = new StringBuilder();
+       for(int i=0;i<s;i++){
+        char ch=command.charAt(i);
+        if(ch=='G'){
+            sb.append("G");
+
+        }
+        else if(ch=='(' && command.charAt(i+1)==')'){
+            sb.append("o");
+            i++;
+        }
+        else{
+            sb.append("al");
+            i+=3;
+        }
+
+       } 
+       return sb.toString();
+    }
+}
