63f5cf017ce19f17e435d5ce2cab047521ac4c93
Archana Mishra <archanamishra082005@gmail.com>
2026-09-18 23:00:46 +0530

    Time: 18 ms (4.47%), Space: 46.2 MB (5.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        return false;
+        String s =Integer.toString(x);
+        String reverse="";
+       
+        for(int i=s.length()-1;i>=0;i--){
+            reverse+=s.charAt(i);
+        }
+        return s.equals(reverse);
+        
+    }
+}
